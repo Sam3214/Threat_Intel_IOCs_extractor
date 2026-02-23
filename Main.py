9e7ef4687f1ca3ac413383e29cf5a3617ab58fd6
Samarth <59115736+Sam3214@users.noreply.github.com>
2026-02-23 18:03:32 +0530

    Refactor IOC extraction to handle multiple articles

--- Main.py
+++ Main.py
@@ -11,23 +11,24 @@ if __name__ == "__main__":
     with open("IOCs/Threat_URL.txt", "w") as file:
             file.write('\n'.join(feeds) + '\n')
 
-    response=ioc_url_get(threat_url[0])
-    print(f"Extracting IOCs from the article: {threat_url[0]}")
-    if response["status"]=="success":
-        print("Found IOCs and extracted successfully.")
-        FILE_HASH_SHA256=response["data"]["FILE_HASH_SHA256"]
-        FILE_HASH_SHA1=response["data"]["FILE_HASH_SHA1"]
-        FILE_HASH_MD5=response["data"]["FILE_HASH_MD5"]
-        if FILE_HASH_SHA256:
-            with open("IOCs/FILE_SHA256.txt", 'w') as file:
-                file.write("\n".join(FILE_HASH_SHA256))
+    for articles in threat_url:
+        response=ioc_url_get(articles)
+        print(f"Extracting IOCs from the article: {articles}")
+        if response["status"]=="success":
+            print("Found IOCs and extracted successfully.")
+            FILE_HASH_SHA256=response["data"]["FILE_HASH_SHA256"]
+            FILE_HASH_SHA1=response["data"]["FILE_HASH_SHA1"]
+            FILE_HASH_MD5=response["data"]["FILE_HASH_MD5"]
+            if FILE_HASH_SHA256:
+                with open("IOCs/FILE_SHA256.txt", 'w') as file:
+                    file.write(f"{articles}:{FILE_HASH_SHA256}\n")
 
-        if FILE_HASH_SHA1:
-            with open("IOCs/FILE_SHA1.txt", "w") as file:
-                file.write("\n".join(FILE_HASH_SHA1))
+            if FILE_HASH_SHA1:
+                with open("IOCs/FILE_SHA1.txt", "w") as file:
+                    file.write(f"{articles}:{FILE_HASH_SHA1}\n")
 
-        if FILE_HASH_MD5:
-            with open("IOCs/FILE_MD5.txt", "w") as file:
-                file.write("\n".join(FILE_HASH_MD5))
-    else:
-        print("No IOCs found or an error occurred during extraction.")
+            if FILE_HASH_MD5:
+                with open("IOCs/FILE_MD5.txt", "w") as file:
+                    file.write(f"{articles}:{FILE_HASH_MD5}\n")
+        else:
+            print("No IOCs found or an error occurred during extraction.")
